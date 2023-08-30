98a188eefc127a4110d533ce60162caf47c346ee
alfiensukma <alfien.sukmaprawira1724@gmail.com>
2023-08-30 14:00:11 +0700

    [Add] Mission 2

--- mission-2/script.js
+++ mission-2/script.js
@@ -1,5 +1,7 @@
 const itemContainer = document.getElementById("itemContainer");
 const cardTemplate = document.getElementById("cardTemplate");
+const cartTableBody = document.getElementById("cartTableBody");
+const totalHargaElem = document.getElementById("totalHarga");
 
 const items = [
     { nama: "Honda Beat", harga: "20500000" },
@@ -36,12 +38,40 @@ function createCard(item) {
         const hargaBarang = item.harga;
         const jumlahBarang = parseInt(jumlahBarangInput.value);
 
+        if (jumlahBarang > 0) {
+            keranjang.push({ nama: item.nama, harga: item.harga, jumlah: jumlahBarang });
+            updateKeranjang();
+        }
+
         console.log("Nama Barang:", namaBarang);
         console.log("Harga Barang:", hargaBarang);
         console.log("Jumlah Barang:", jumlahBarang);
     });
 
     return cardClone;
+}
+
+function updateKeranjang() {
+    cartTableBody.innerHTML = "";
+    
+    let totalHarga = 0;
+    keranjang.forEach(item => {
+        const hargaBarang = item.harga * item.jumlah;
+        totalHarga += hargaBarang;
+        
+        const row = cartTableBody.insertRow();
+        const namaCell = row.insertCell(0);
+        const jumlahCell = row.insertCell(1);
+        const hargaCell = row.insertCell(2);
+        const totalCell = row.insertCell(3);
+        
+        namaCell.textContent = item.nama;
+        jumlahCell.textContent = item.jumlah;
+        hargaCell.textContent = `Rp. ${item.harga}`;
+        totalCell.textContent = `Rp. ${hargaBarang}`;
+    });
+    
+    totalHargaElem.textContent = `Rp. ${totalHarga}`;
 }
 
 items.forEach(item => {
